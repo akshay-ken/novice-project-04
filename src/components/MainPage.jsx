4e5c0f0116228a07d625b7cf4d7af52fa6c305f4
akshay-ken <akshaybest3120@gmail.com>
2025-07-12 16:06:30 +0530

    project done now practicing useEffect hooks cleanup

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import lineIcon from "../assets/images/pattern-divider-mobile.svg";
+import lineIconDesktop from "../assets/images/pattern-divider-desktop.svg";
 
 async function fetchAdvice() {
   const adviceApi = await fetch("https://api.adviceslip.com/advice");
@@ -14,21 +15,31 @@ async function fetchAdvice() {
 
 export function MainPage() {
   const [currentAdvice, setCurrentAdvice] = useState([]);
-  useEffect(function () {
-    fetchAdvice()
-      .then((advice) => setCurrentAdvice(advice))
-      .catch((error) => {
-        console.log("failed to fetch advice");
-      });
-  }, []);
-  console.log(currentAdvice);
-  function handleFetchNewAdvice() {
-    fetchAdvice();
+
+  async function fetchAndSetAdvice() {
+    try {
+      const advice = await fetchAdvice();
+      setCurrentAdvice(advice);
+    } catch (err) {
+      console.error("Failed to fetch advice:", err);
+    } finally {
+      console.log("try catch block done running.");
+    }
   }
+  useEffect(function () {
+    fetchAndSetAdvice();
+  }, []);
+
+  console.log(currentAdvice);
+
+  function handleClick() {
+    fetchAndSetAdvice();
+  }
+
   return (
     <>
       <main className="w-full flex flex-col justify-center items-center h-screen">
-        <section className="bg-Blue-950 max-w-sm flex flex-col p-12 rounded-xl">
+        <section className="bg-Blue-950 max-w-sm md:max-w-lg flex flex-col p-12 rounded-xl">
           <h1 className="text-base uppercase tracking-[0.4rem] text-center mb-6 text-Green-300 font-medium">
             Advice # {currentAdvice.id}
           </h1>
@@ -38,14 +49,15 @@ export function MainPage() {
             </p>
           </article>
 
-          <img src={lineIcon} className="my-8 " alt="" />
+          <img src={lineIcon} className="my-8 md:hidden " alt="" />
+          <img src={lineIconDesktop} className="my-8 md:block hidden" alt="" />
 
           <button
             className="p-8 bg-Green-300 mt-auto -mb-20 mx-auto w-fit rounded-full 
           bg-[url('./assets/images/icon-dice.svg')] bg-no-repeat bg-center
            shadow-Green-300 active:shadow-[0px_0px_10px_10px] focus:shadow-[0px_0px_10px_10px]
           "
-            onClick={handleFetchNewAdvice}
+            onClick={handleClick}
           ></button>
         </section>
       </main>
